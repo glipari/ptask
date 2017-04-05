4290e903a29ee7134044b2e1be7948cda4e12f93
Giuseppe Lipari <g.lipari@sssup.it>
2017-04-05 13:10:54 +0200

    Added error message in calibrate.c

--- tests/calibrate.c
+++ tests/calibrate.c
@@ -1,10 +1,16 @@
 #include "calibrate.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 long calibrate()
 {
-    FILE *f = fopen(CALIBRATE_FILE, "r");
     long v;
+    FILE *f = fopen(CALIBRATE_FILE, "r");
+    if (f == NULL) {
+        perror("Cannot open calibration file");
+        printf("Run tests/wcet_calibration to generate the calibration file\n");
+        exit(-1);
+    }
     fscanf(f, "%ld", &v);
     fclose(f);
     return v;
